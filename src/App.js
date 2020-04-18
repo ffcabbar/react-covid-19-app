7f3e3b07aa4423134cf0afcdb5bf0b27e83974f1
ffcabbar <ffcabbar@gmail.com>
2020-04-18 19:37:05 +0300

    new 2 link added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
 import image from "../src/images/image.png";
+import { Typography, Link } from "@material-ui/core";
 
 class App extends React.Component {
   state = {
@@ -30,9 +31,31 @@ class App extends React.Component {
     return (
       <div className={styles.container}>
         <img src={image} className={styles.image} alt="COVID-19" />
+        <Typography style={{marginTop: "20px"}} >
+          Data sources from{" "}
+          <Link
+            href="https://github.com/alankilalank/react-covid-19"
+            style= {{color: "#f1bb07"}}
+            target="_blank"
+          >
+            mathdroid's
+          </Link>{" "}
+          API
+        </Typography>
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} />
+        <Typography style={{marginTop: "30px"}}>
+          Made With <span role="img" aria-label="love">🧡</span> by{" "}
+          <Link
+            href="https://www.linkedin.com/in/ffcabbar/"
+            className={styles.api}
+            target="_blank"
+            style= {{color: "#f1bb07"}}
+          >
+            Furkan
+          </Link>
+        </Typography>
       </div>
     );
   }
