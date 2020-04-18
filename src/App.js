3ee617810383497da77237def9689f68baf502de
ffcabbar <ffcabbar@gmail.com>
2020-04-18 18:27:39 +0300

    netlify

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends React.Component {
 
     return (
       <div className={styles.container}>
+        netfliyyyyy
         <img src={image} className={styles.image} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
